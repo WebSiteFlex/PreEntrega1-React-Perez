e6dd68af79267ecaea7aa8d3a50f96c6816f8fa6
Julian Perez <julianperez620@gmail.com>
2023-12-28 13:45:57 -0300

    deleted files

--- PreEntrega1-Perez/SimplePage/src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import Classes from "../Button/Button.module.css";
-
-const Button =  (props) =>{
-    return (
-            <button onClick={props.click} className={Classes.navButton} >{props.label}</button>     
-    )
-};
-
-export default Button;
