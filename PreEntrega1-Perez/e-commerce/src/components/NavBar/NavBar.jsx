8e1088b036c70243fd118281111c858328c0a328
Julian Perez <julianperez620@gmail.com>
2023-12-30 21:27:40 -0300

    modificcations

--- PreEntrega1-Perez/e-commerce/src/components/NavBar/NavBar.jsx
+++ PreEntrega1-Perez/e-commerce/src/components/NavBar/NavBar.jsx
@@ -1,17 +1,19 @@
 import Button from "../ItemListContainer/ItemListContainer"
 
-const NavBar = () =>{
+const NavBar = () => {
     return (
         <>
-        <Button label={"Home"} click={()=> {
-            alert("you clicked in Home!")
-        }}></Button>
-        <Button label={"Contact"} click={()=> {
-            alert("you clicked in Contact!")
-        }}></Button>
-        <Button label={"About us"} click={()=> {
-            alert("you clicked in About us!")
-        }}></Button>
+
+            <h1>Simple Site </h1>
+            <Button label={"Home"} click={() => {
+                alert("you clicked in Home!")
+            }}></Button>
+            <Button label={"Contact"} click={() => {
+                alert("you clicked in Contact!")
+            }}></Button>
+            <Button label={"About us"} click={() => {
+                alert("you clicked in About us!")
+            }}></Button>
         </>
     )
 }
